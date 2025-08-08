bc08e9002de9a86c00983d15453b0e27b219f898
Faiyaz Alam <faiyaz0399@gmail.com>
2025-08-08 11:30:15 +0530

    h

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-	origin: ['http://localhost:5173','https://mern-blog-coral.vercel.app'],
+	origin: ['http://localhost:5173','*'],
 	credentials: true
 }))
 
